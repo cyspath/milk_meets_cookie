8738fc84a5f1535ab6dc416d3c40e62a4e09d508
Michael Li <lvlichaelly@gmail.com>
2017-03-18 23:42:40 -0700

    [remove] old files and add chats model

---
+++ server/models/chats.js
@@ -0,0 +1,15 @@
+module.exports = function(sequelize, DataTypes) {
+  const Chat = sequelize.define('Chat', {
+    id:                         { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    sender_id:                  { type: DataTypes.INTEGER },
+    receiver_id:                { type: DataTypes.INTEGER },
+    message:                    { type: DataTypes.STRING },
+  }, {
+    underscored: true,
+    tableName: 'chats',
+    indexes: [
+      { method: 'BTREE', fields: ['sender_id', 'receiver_id'] },
+    ],
+  });
+  return Chat;
+};
--- server/models/like.js
+++ server/models/like.js
@@ -5,7 +5,6 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     underscored: true,
     defaultScope: { where: { active: true },
-  },
   });
   return Like;
 };
