01bd563ce3a39d888c993a8d101518cb690abb6a
Michael Li <lvlichaelly@gmail.com>
2017-04-01 21:34:43 -0700

    [update] rds env variables

--- server/config.js
+++ server/config.js
@@ -12,11 +12,11 @@ module.exports = {
   },
   "production": {
     jwtSecret:  process.env.JWT_SECRET,
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host":     process.env.DB_HOST,
-    "port":     process.env.DB_PORT,
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DB_NAME,
+    "host":     process.env.RDS_HOSTNAME,
+    "port":     process.env.RDS_PORT,
     "dialect": "mysql"
   },
   "test": {
